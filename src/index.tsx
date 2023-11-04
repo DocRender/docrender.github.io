5af3c5fd61a067348fe31a7db60d8eedcd5db0b5
Huajian Mao <huajianmao@gmail.com>
2023-11-04 12:38:56 +0800

    init: create with cra

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "antd";
+import dayjs from "dayjs";
+
+import App from "./pages/home";
+import reportWebVitals from "./reportWebVitals";
+
+import zhCN from "antd/locale/zh_CN";
+import "dayjs/locale/zh-cn";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+dayjs.locale("zh-cn");
+
+root.render(
+  <React.StrictMode>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
